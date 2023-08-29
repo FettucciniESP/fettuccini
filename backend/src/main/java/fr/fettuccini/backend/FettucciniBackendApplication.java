c42469337b03aefcc6087b4378d24382c8c95efa
mgl <marius.glo@protonmail.com>
2023-08-29 10:59:23 +0200

    feat: backend init

---
+++ backend/src/main/java/fr/fettuccini/backend/FettucciniBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.fettuccini.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FettucciniBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FettucciniBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/fettuccini/backend/FettucciniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fettuccini.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FettucciniBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
