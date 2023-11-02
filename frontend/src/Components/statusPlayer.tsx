a7fc3a55782c9158304bc610f780594eeda101bb
Mathis <mathis.ragot@epitech.eu>
2023-11-02 15:36:28 +0100

    [US-198] Add status player View

--- frontend/src/Components/statusPlayer.tsx
+++ frontend/src/Components/statusPlayer.tsx
@@ -1,5 +1,58 @@
-import { Box } from '@chakra-ui/react'
+import { GameAction } from '@/app/models/Game'
+import { GameInfo } from '@/app/models/Player'
+import { Box, Text } from '@chakra-ui/react'
 
-export default function StatusPlayer() {
-    return
+export default function StatusPlayer(gameInfo: GameInfo) {
+  return (
+    <Box
+      w={'30%'}
+      bg="#F7F0E1"
+      borderRadius={5}
+      paddingInline={2}
+      paddingBlock={2}
+    >
+      <Box
+        h={60}
+        bg="gray"
+        display="flex"
+        borderTopRadius={5}
+        alignItems={'center'}
+        justifyContent={'center'}
+        marginBottom={2}
+      >
+        <Text as={'b'} fontSize={22}>
+          Joueurs
+        </Text>
+      </Box>
+      <Box
+        bg="gray"
+        display="flex"
+        flexDirection={'column'}
+        paddingBlock={30}
+        borderBottomRadius={5}
+        paddingLeft={30}
+      >
+        {gameInfo.players.map((value, index) => (
+          <Box
+            key={index}
+            paddingBottom={10}
+            display={'flex'}
+            flexDirection={'row'}
+          >
+            <Text>Siège {value.siege}</Text>
+            <Text paddingInline={20}>
+              {value.lastAction == GameAction.BET
+                ? 'IMG'
+                : value.lastAction == GameAction.CHECK
+                ? 'ok'
+                : value.lastAction == GameAction.FOLD
+                ? 'X'
+                : ' '}
+            </Text>
+            <Text> {value.lastAction}</Text>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  )
 }
