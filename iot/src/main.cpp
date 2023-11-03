bf0c65ae6446cd649b36df2003ec67cae1a69937
Clement <c.boesmier@aptatio.com>
2023-11-03 16:25:33 +0100

    add post example OKAY

--- iot/src/main.cpp
+++ iot/src/main.cpp
@@ -38,32 +38,53 @@ IPAddress myIP(192, 168, 0, 2);
 IPAddress myGW(192, 168, 0, 254);
 IPAddress mySN(255, 255, 255, 0);
 
-void setup()
-{
-  Serial.begin(115200);
+void setup(){
+    Serial.begin(115200);
 
-  while (!Serial);
+    while (!Serial);
+    delay(2000);
 
-  // Using this if Serial debugging is not necessary or not using Serial port
-  //while (!Serial && (millis() < 3000));
+    // Using this if Serial debugging is not necessary or not using Serial port
+    //while (!Serial && (millis() < 3000));
 
-  Serial.print("\nStarting BasicHttpClient on " + String(ARDUINO_BOARD));
-  Serial.println(" with " + String(SHIELD_TYPE));
-  Serial.println(WEBSERVER_WT32_ETH01_VERSION);
+    Serial.print("\nStarting BasicHttpClient on " + String(ARDUINO_BOARD));
+    Serial.println(" with " + String(SHIELD_TYPE));
+    Serial.println(WEBSERVER_WT32_ETH01_VERSION);
 
-  // To be called before ETH.begin()
-  WT32_ETH01_onEvent();
+    // To be called before ETH.begin()
+    WT32_ETH01_onEvent();
 
-  //bool begin(uint8_t phy_addr=ETH_PHY_ADDR, int power=ETH_PHY_POWER, int mdc=ETH_PHY_MDC, int mdio=ETH_PHY_MDIO,
-  //           eth_phy_type_t type=ETH_PHY_TYPE, eth_clock_mode_t clk_mode=ETH_CLK_MODE);
-  //ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER, ETH_PHY_MDC, ETH_PHY_MDIO, ETH_PHY_TYPE, ETH_CLK_MODE);
-  ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER);
+    //bool begin(uint8_t phy_addr=ETH_PHY_ADDR, int power=ETH_PHY_POWER, int mdc=ETH_PHY_MDC, int mdio=ETH_PHY_MDIO,
+    //           eth_phy_type_t type=ETH_PHY_TYPE, eth_clock_mode_t clk_mode=ETH_CLK_MODE);
+    //ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER, ETH_PHY_MDC, ETH_PHY_MDIO, ETH_PHY_TYPE, ETH_CLK_MODE);
+    ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER);
 
-  // Static IP, leave without this line to get IP via DHCP
-  //bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = 0, IPAddress dns2 = 0);
-  ETH.config(myIP, myGW, mySN);
+    // Static IP, leave without this line to get IP via DHCP
+    //bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = 0, IPAddress dns2 = 0);
+    ETH.config(myIP, myGW, mySN);
 
-  WT32_ETH01_waitForConnect();
+    WT32_ETH01_waitForConnect();
+
+    delay(1000);
+
+    if(WT32_ETH01_isConnected()){
+        HTTPClient http;
+        http.begin("http://192.168.0.1:8090/api/collections/card_log/records");
+        http.addHeader("Content-Type", "application/json");
+        int httpCode = http.POST("{\"card_id\": \"15\"}");
+        Serial.print("[HTTP] POST...\n");
+        if (httpCode > 0){
+            // HTTP header has been send and Server response header has been handled
+            Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+            // file found at server
+            if (httpCode == HTTP_CODE_OK){
+                String payload = http.getString();
+                Serial.println(payload);
+            }
+        } else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+    }
 }
 
 void loop()
@@ -102,5 +123,5 @@ void loop()
     http.end();
   }
 
-  delay(5000);
+  delay(10000);
 }
