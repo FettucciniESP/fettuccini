fa7dddf6bdc838f3e5689e649774a594d0fa8e9f
Clement <c.boesmier@aptatio.com>
2023-11-24 16:04:47 +0100

    add nfc read

--- iot/src/Program.cpp
+++ iot/src/Program.cpp
@@ -1,15 +1,38 @@
 #include "Program.h"
+#include <WiFi.h>
+
+void initWiFi() {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WSSID, PASS);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(WiFi.status());
+        delay(1000);
+    }
+    Serial.println(WiFi.localIP());
+}
+
 
 Program::Program() {
     // Startup
     Serial.begin(MONITOR_SPEED);
 
-    this->screen = new OledScreen(OLED_WIDTH, OLED_HEIGHT, OLED_RESET);
+    nfc = new NfcModule(27, 26);
 
-    this->screen->welcome();
+    //this->screen = new OledScreen(OLED_WIDTH, OLED_HEIGHT, OLED_RESET);
 
+    initWiFi();
+
+
+//    this->screen->welcome();
 }
 
+String oldVal = "";
+
 void Program::loop() {
-    // Loop
+    String val = nfc->read();
+    if(val !="" && val != oldVal){
+        Serial.println(val);
+        oldVal = val;
+    }
 }
