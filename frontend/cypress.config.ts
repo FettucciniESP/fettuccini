075436e465e2e63e90b0665f10fc171feb988ccd
mgl <marius.glo@protonmail.com>
2023-11-28 13:54:34 +0100

    fix: temporary dont run spec unit

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
-      on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+      //on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
 
       return config
     },
