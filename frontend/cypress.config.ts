c1b2f694da335619874532758be282d0c72d558c
Nathan <nathan.pignon79@gmail.com>
2024-02-18 14:42:33 +0100

    [TECH] Update cypress configuration to accept scss with next

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "cypress";
+import path from "path";
 
 export default defineConfig({
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
+      webpackConfig: {
+        resolve: {
+          alias: {
+            '@': path.resolve(__dirname, './src'),
+          },
+        },
+      }
     },
   },
 
