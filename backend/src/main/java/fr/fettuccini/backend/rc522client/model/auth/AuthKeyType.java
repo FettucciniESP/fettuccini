0c85f1805e1b1baf1da1c8800b4c4a1016ea4e4e
Marius <marius.glo@protonmail.com>
2024-03-01 16:56:56 +0100

    allez hop ça dégage (#44)

--- backend/src/main/java/fr/fettuccini/backend/rc522client/model/auth/AuthKeyType.java
+++
@@ -1,5 +0,0 @@
-package fr.fettuccini.backend.rc522client.model.auth;
-
-public enum AuthKeyType {
-	AUTH_A, AUTH_B
-}
--- backend/src/main/java/fr/fettuccini/backend/rc522client/model/communication/CommunicationStatus.java
+++
@@ -1,5 +0,0 @@
-package fr.fettuccini.backend.rc522client.model.communication;
-
-public enum CommunicationStatus {
-	SUCCESS, NO_TAG, AUTH_ERROR, ERROR
-}
