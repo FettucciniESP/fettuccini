9c485b0d465efca79a32446f1c23a76f04716a86
Clement <c.boesmier@aptatio.com>
2024-03-07 10:58:34 +0100

    add 2nd reader

--- iot/src/main.cpp
+++ iot/src/main.cpp
@@ -21,31 +21,44 @@
 
 
 NfcCardReader* card1;
+NfcCardReader* card2;
 
 bool card1Connected = 0;
+bool card2Connected = 0;
 
 void setup(void) {
     Serial.begin(MONITOR_SPEED);
 
     delay(2000);
     Serial.println("Hello!");
-    // card1 = new NfcCardReader(SDA_1, SCL_1, 400000U);
 
-    Serial2.begin(115200,SERIAL_8N1, SDA_1, SCL_1);
+    // Serial2.begin(115200,SERIAL_8N1, SDA_1, SCL_1);
+    // card1 = new NfcCardReader(Serial2);
+    // while (!card1Connected){
+    //     card1Connected = card1->connect();
+    // }
 
-    card1 = new NfcCardReader(Serial2);
-
-    while (!card1Connected)
-    {
-        card1Connected = card1->connect();
+    Serial1.begin(115200,SERIAL_8N1, SDA_2, SCL_2);
+    Serial.print(SDA_2);
+    Serial.println(SCL_2);
+    delay(100);
+    card2 = new NfcCardReader(Serial1);
+    while (!card2Connected){
+        card2Connected = card2->connect();
     }
 
 }
 
 void loop(void) {
-    String val = card1->read();
-    if(val != ""){
-        Serial.print("new card : ");
-        Serial.println(val);
+    // String val1 = card1->read();
+    // if(val1 != ""){
+    //     Serial.print("new card 1 : ");
+    //     Serial.println(val1);
+    // }
+
+    String val2 = card2->read();
+    if(val2 != ""){
+        Serial.print("new card 2 : ");
+        Serial.println(val2);
     }
 }
