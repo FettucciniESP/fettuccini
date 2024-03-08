499fefe17283bb78ff1820109f3f3e0ce89aaccd
ValentinM <val.messiant@gmail.com>
2024-03-08 10:55:14 +0100

    [US-291] Adding new TU

--- frontend/cypress/components/calculator-modal/Calculator.cy.tsx
+++ frontend/cypress/components/calculator-modal/Calculator.cy.tsx
@@ -1,31 +1,62 @@
 import React from 'react'
 import Calculator from '@/app/components/calculator-modal/Calculator';
-import useActionButtons from '@/app/components/information-panel/game-informations/action-buttons/useActionButtons';
-import { GameActionEnum } from '@/app/enums/GameAction.enum';
 
-describe('Blind informations', () => {
+describe('Calculator', () => {
   const playerInfos = {
     name: "string",
     seatIndex: 1,
     balance: 1000
   }
 
-  const {
-    handleActionButtonClick,
-    buttonIsDisabled,
-} = useActionButtons()
-
+  
   beforeEach(() => {
-    cy.mount(<Calculator openCalculator={true} handleActionButtonClick={(amount: number) => handleActionButtonClick(playerInfos, GameActionEnum.BET, amount)} />)
+    const handleActionButtonClickStub = cy.stub().as('handleActionButtonClickStub');
+    cy.mount(<Calculator openCalculator={true} handleActionButtonClick={handleActionButtonClickStub} />)
   });
 
+
   it('renders correctly', () => {
-    for (let i = 1; i <= 9; i += 3) {
+    // Check if the calculator is visible
+    for (let i = 0; i <= 9; i += 1) {
       cy.get('[id="idButtonNumber'+i+'"]').should('be.visible');
-      cy.get('[id="idButtonNumber'+(i+1)+'"]').should('be.visible');
-      cy.get('[id="idButtonNumber'+(i+2)+'"]').should('be.visible');
     }
-    cy.get('[id="idButtonNumber0"]').should('be.visible');
     cy.get('[id="idButtonDelete"]').should('be.visible');
+  });
+
+
+  it('Text is modified after button click', () => {
+    // Check if the text is empty
+    cy.get('[id="idResult"]').should('have.value', '');
+
+    // Click on all buttons
+    for (let i = 0; i <= 9; i += 1) {
+      cy.get('[id="idButtonNumber'+i+'"]').click();
+    }
+
+    // Check if the text is modified
+    cy.get('[id="idResult"]').should('have.value', '0123456789');
+
+    // Delete the last number
+    cy.get('[id="idButtonDelete"]').click();
+
+    // Check if the text is modified
+    cy.get('[id="idResult"]').should('have.value', '012345678');
+  });
+
+
+  it('Submit the value', () => {
+    // Check if the text is empty
+    cy.get('[id="idResult"]').should('have.value', '');
+
+    // Click on all buttons
+    for (let i = 0; i <= 9; i += 1) {
+      cy.get('[id="idButtonNumber'+i+'"]').click();
+    }
+
+    // Submit the value
+    cy.get('[id="idButtonSubmit"]').click();
+
+    // Check if the value is submitted
+    cy.get('@handleActionButtonClickStub').should('be.calledWith', parseInt('0123456789'));
   });
 })
