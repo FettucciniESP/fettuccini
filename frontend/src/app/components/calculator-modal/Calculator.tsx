6833f0777052bfa704ea3effa115b2ca7657c707
ValentinM <val.messiant@gmail.com>
2024-03-14 13:53:05 +0100

    [US-291] Updating calculator modal css

--- frontend/src/app/components/calculator-modal/Calculator.tsx
+++ frontend/src/app/components/calculator-modal/Calculator.tsx
@@ -38,10 +38,10 @@ export default function Calculator({openCalculator, closeCalculator, handleNumbe
     for (let i = 1; i <= 9; i += 3) {
       buttons.push(
         <Flex key={i} mt={2}>
-          <Button id={"idButtonNumber"+i} onClick={() => handleChange(i)} mr={2} className={styles.ButtonNumber}>
+          <Button id={"idButtonNumber"+i} onClick={() => handleChange(i)} className={styles.ButtonNumber}>
             {i}
           </Button>
-          <Button id={"idButtonNumber"+(i+1)} onClick={() => handleChange(i + 1)} mr={2} className={styles.ButtonNumber}>
+          <Button id={"idButtonNumber"+(i+1)} onClick={() => handleChange(i + 1)} className={styles.ButtonNumber}>
             {i + 1}
           </Button>
           <Button id={"idButtonNumber"+(i+2)} onClick={() => handleChange(i + 2)} className={styles.ButtonNumber}>
@@ -52,7 +52,7 @@ export default function Calculator({openCalculator, closeCalculator, handleNumbe
     }
     buttons.push(
       <Flex key={0} mt={2}>
-        <Button id="idButtonNumber0" onClick={() => handleChange(0)} mr={10}  className={styles.ButtonNumber}>
+        <Button id="idButtonNumber0" onClick={() => handleChange(0)} className={styles.ButtonZero}>
           0
         </Button>
         <Button id="idButtonDelete" onClick={handleDelete} leftIcon={<ArrowBackIcon />} className={styles.ButtonNumber}>
@@ -63,24 +63,22 @@ export default function Calculator({openCalculator, closeCalculator, handleNumbe
   };
 
   return ( 
-    <Box>
-      <Modal isOpen={openCalculator} onClose={() => closeCalculator()}>
-        <ModalOverlay />
-        <ModalContent className={styles.CalculatorContent}>
-          <ModalCloseButton color="#F7F0E1"/>
-          <ModalBody className={styles.CalculatorBody}>
-            <Flex direction="column" align="center">
-              <Box mb={4}>
-                <input id="idResult" value={code} disabled className={styles.inputCalculator}/>
-              </Box>
-              {renderButtons()}
-              <Button id="idButtonSubmit" onClick={handleSubmit} mt={4} className={styles.ButtonSubmit}>
-                Valider
-              </Button>
-            </Flex>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </Box>
+    <Modal isOpen={openCalculator} onClose={() => closeCalculator()} isCentered size={'sm'}>
+      <ModalOverlay />
+      <ModalContent className={styles.CalculatorContent} maxW="40vw">
+        <ModalCloseButton color="#F7F0E1"/>
+        <ModalBody className={styles.CalculatorBody}>
+          <Flex direction="column" align="center">
+            <Box mb={2}>
+              <input id="idResult" value={code} disabled className={styles.inputCalculator}/>
+            </Box>
+            {renderButtons()}
+            <Button id="idButtonSubmit" onClick={handleSubmit} mt={2} className={styles.ButtonSubmit}>
+              Valider
+            </Button>
+          </Flex>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   );
 };
